215e1717b10e7a1a9c0d4190d4558dcaf9d05f4e
nycholassousa <nycholas@live.com>
2023-01-27 17:02:21 -0300

    Adicionando interface e ajustando yaml do plugin para editar snippets

---
+++ mapstruct/templates/app/src/main/{{language}}/{{path}}/adapter/mapper/BasicMapper.java
@@ -0,0 +1,9 @@
+package {{group}}.{{project_name|replace('-','')}}.adapter.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface BasicMapper {
+}
